0656ee16305f3b64771c50a25481e6d15f4d1e29
mmtg445 <mmtg445@gmail.com>
2024-12-12 00:23:58 +0600

    Update info.py

--- info.py
+++ info.py
@@ -15,7 +15,7 @@ def is_enabled(value, default):
 #main variables
 API_ID = int(environ.get('API_ID', '21188057'))
 API_HASH = environ.get('API_HASH', '8564fab8db759bb04b1907bd12ed98ef')
-BOT_TOKEN = environ.get('BOT_TOKEN', '')
+BOT_TOKEN = environ.get('BOT_TOKEN', '7870422082:AAE3F_sNVoHeBFwwgaCOW6gZLTWb0Tm5xNo')
 
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6976744562 6085825994 8102446291').split()]
 USERNAME = environ.get('USERNAME', "https://telegram.me/RahatVx")
